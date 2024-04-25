528708ee6684994e6e768ef4d1f1c434a21d1e81
sivasankar5456 <sivasankar5456@gmail.com>
2024-04-25 08:05:14 +0530

    button functionality added

--- src/components/ButtonTag.jsx
+++ src/components/ButtonTag.jsx
@@ -1,6 +1,15 @@
-const ButtonTag = (props) => {
+const ButtonTag = ({button_name, selectTag,selected}) => {
+
+    const tagStyle = {
+        Java: {backgroundColor: "#fda821"}, 
+        NodeJs: {backgroundColor: "#15d4c8"},
+        React: {backgroundColor: "#ffd12c"},
+        MongoDB: {backgroundColor: "#4cdafc"},
+        default: {backgroundColor: "#f9f9f9"},
+      };
+
     return (
-        <button className='tag'>{props.button_name}</button>
+        <button type="button" className='tag' onClick={() => selectTag(button_name)}  style={selected? tagStyle[button_name]: tagStyle.default} >{button_name}</button>
     );
   };
   export default ButtonTag
--- src/components/TaskManagementForm.jsx
+++ src/components/TaskManagementForm.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskManagementForm.css";
 import ButtonTag from "./ButtonTag";
 
 
 const TaskManagementForm = () => {
+  
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+    tags: []
+  })
+
+  const selectTag = (button_name) => {
+    // console.log(button_name);
+    if(taskData.tags.some(item => item === button_name)) {
+      const filterTags = taskData.tags.filter(item => item !== button_name);
+      setTaskData(prev => {
+        return {...prev, tags: filterTags};
+      })
+    } else {
+      setTaskData(prev => {
+        return {...prev, tags: [...prev.tags, button_name]}
+      })
+    }
+  };
+  const checkTag = (name) => {
+    return taskData.tags.some(item => item === name);
+  };
+
+  const handleChange = (e) => {
+    // const name = e.target.name;
+    // const value = e.target.value;
+   
+    const {name, value} = e.target
+    setTaskData (prev => {
+      return {...prev, [name]: value}
+    })
+  }
+  console.log(taskData.tags);
+  const handleTaskChange = e => {
+    setTask(e.target.value)
+  };
+  const handleStatusChange = e => {
+    setStatus(e.target.value)
+  };
+
   return (
     <header className="app_header">
       <form>
@@ -11,19 +52,16 @@ const TaskManagementForm = () => {
           type="text"
           className="task_input"
           placeholder="Enter your task"
+          onChange={handleChange}
         />
         <div className="task_form_bottom_line">
-                {/* <button className='tag'>Java</button>
-                <button className='tag'>Python</button>
-                <button className='tag'>React</button>
-                <button className='tag'>MongoDB</button>    */}
                 
-          <ButtonTag button_name="Java" />
-          <ButtonTag button_name="Mongo DB" />
-          <ButtonTag button_name="React" />
-          <ButtonTag button_name="Node Js" />
+          <ButtonTag button_name="Java" selectTag={selectTag} selected={checkTag("Java")} />
+          <ButtonTag button_name="MongoDB" selectTag={selectTag} selected={checkTag("MongoDB")} />
+          <ButtonTag button_name="React" selectTag={selectTag} selected={checkTag("React")} />
+          <ButtonTag button_name="NodeJs" selectTag={selectTag} selected={checkTag("NodeJs")} />
 
-          <select className="task_status">
+          <select className="task_status" onChange={handleChange} >
             <option value="todo">To do</option>
             <option value="inprogress">In progress</option>
             <option value="done">Done</option>
